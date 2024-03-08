8d38990017ce2cb9a66a3e4f6f01871c915e9b57
Rebecca McKenzie <63881668+RMcKenzie111@users.noreply.github.com>
2024-03-08 00:47:05 +0000

    have root and from currency

--- app.rb
+++ app.rb
@@ -1,9 +1,58 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  api_url = "http://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+
+  raw_data = HTTP.get(api_url)
+
+  raw_data_string = raw_data.to_s
+
+  parsed_data = JSON.parse(raw_data_string)
+
+  #get symbols from the JSON
+  @symbols = parsed_data.fetch("currencies").keys
+
+  #@symbols.each do |list_symbols|
+   # @list_symbols = list_symbols
+  #end
+
+  #1.times do
+  #  @list_symbols.push(@symbols)
+  #end
+
+  #render view template to show symbols
+  erb(:home)
+end
+
+get("/:from_currency") do
+  @original_currency = params.fetch("from_currency")
+
+  api_url = "http://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+
+  # some more code to parse the URL and render a view template
+  raw_data = HTTP.get(api_url)
+
+  raw_data_string = raw_data.to_s
+
+  parsed_data = JSON.parse(raw_data_string)
+
+  #get symbols from the JSON
+  @symbols = parsed_data.fetch("currencies").keys
+
+  erb(:dynmc_from_currency)
+
+end
+
+get("/:from_currency/:to_currency") do
+  @original_currency = params.fetch("from_currency")
+  @destination_currency = params.fetch("to_currency")
+
+  "http://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+
+  # some more code to parse the URL and render a view template
+
+
+
 end
